1290f7887a1741cbe08f39ac40f3d8b91f2f3a9e
Mike <michael.nyberghjerto@outlook.com>
2017-01-18 23:36:43 +0100

    finished start view and key event

--- app/src/main/java/com/mikecoding/ballman/main.java
+++ app/src/main/java/com/mikecoding/ballman/main.java
@@ -8,6 +8,7 @@ import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Display;
+import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
@@ -228,4 +229,18 @@ public class main extends AppCompatActivity {
 
         return true;
     }
+
+    //disable return button
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event){
+        if(event.getAction() == KeyEvent.ACTION_DOWN){
+            switch (event.getKeyCode()){
+                case KeyEvent.KEYCODE_BACK:
+                    return true;
+            }
+        }
+
+        return super.dispatchKeyEvent(event);
+    }
+
 }
--- app/src/main/java/com/mikecoding/ballman/result.java
+++ app/src/main/java/com/mikecoding/ballman/result.java
@@ -1,7 +1,12 @@
 package com.mikecoding.ballman;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
 import android.widget.TextView;
 
 public class result extends AppCompatActivity {
@@ -15,6 +20,38 @@ public class result extends AppCompatActivity {
         TextView highScoreLabel = (TextView) findViewById(R.id.highScoreLabel);
 
         int score = getIntent().getIntExtra("SCORE", 0);
+        scoreLabel.setText(score + "");
+
+        SharedPreferences settings = getSharedPreferences("GAME_DATA", Context.MODE_PRIVATE);
+        int highScore = settings.getInt("HIGH_SCORE", 0);
+
+        if(score > highScore){
+            highScoreLabel.setText("High Score : " + score);
+
+            //save
+            SharedPreferences.Editor editor = settings.edit();
+            editor.putInt("HIGH_SCORE", score);
+
+        }else{
+            highScoreLabel.setText("High Score : " + highScore);
+        }
 
     }
+
+    public void tryAgain(View view){
+        startActivity(new Intent(getApplicationContext(), start.class));
+    }
+    //disable return button
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event){
+        if(event.getAction() == KeyEvent.ACTION_DOWN){
+            switch (event.getKeyCode()){
+                case KeyEvent.KEYCODE_BACK:
+                    return true;
+            }
+        }
+
+        return super.dispatchKeyEvent(event);
+    }
+
 }
